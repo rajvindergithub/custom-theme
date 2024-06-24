fbd6b955ef546050746607c777a4aa8b3392011d
rajvinder_rtds <rajvinder.singh@myrealdata.in>
2024-06-24 13:26:36 +0530

    wordpress theme basic files

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+//Add Theme Support
+
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post-format', ['aside','gallery','image','link','quote','status','video','audio','chat'] );
+
+add_theme_support( 'html5' );
+add_theme_support( 'automatic-feed-links' );
+add_theme_support( 'custom-background' );
+
+
+
+?>
